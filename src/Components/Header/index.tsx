ee6a3b4e4b73b117f43d8ec084efadf97a0066e0
thiago <thborba1@gmail.com>
2024-02-17 19:40:01 -0300

    feat: melhora componentização do Header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,28 +1,11 @@
-import { FlexBox } from '../FlexBox';
-import styles from './styles.module.scss';
+import './styles.module.scss';
+import { HeaderTabs } from '@/Components';
 
 export function Header() {
   return (
-    <header className={styles['container']}>
-      <FlexBox alignItems="center">
-        <FlexBox alignItems="center" className={styles['sider-bar-title']}>
-          <span>Thiago Borba</span>
-        </FlexBox>
-        <FlexBox>
-          <a href="home">_hello</a>
-        </FlexBox>
-        <FlexBox>
-          <a href="services">_projects</a>
-        </FlexBox>
-
-        <FlexBox>
-          <a href="about">_about</a>
-        </FlexBox>
-      </FlexBox>
-
-      <FlexBox alignItems="center">
-        <a href="#contact">_contact</a>
-      </FlexBox>
+    <header>
+      <span>Thiago Borba</span>
+      <HeaderTabs />
     </header>
   );
 }
