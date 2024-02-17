ebbd5eff5ef73a45679fb3a56ba0a52974eca05c
thiago <thborba1@gmail.com>
2024-02-17 06:48:36 -0300

    feat: Cria footer

---
+++ src/Components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss';
+
+export function Footer() {
+  return (
+    <footer className={styles['footer']}>
+      <p>Footer</p>
+    </footer>
+  );
+}
+
+export default Footer;
