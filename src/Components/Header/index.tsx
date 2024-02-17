96c462ef35f413dbc3ae2d252b52d36402f136fc
thiago <thborba1@gmail.com>
2024-02-17 06:50:17 -0300

    feat: renomeia e estiliza header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,18 +1,30 @@
 import { FlexBox } from '../FlexBox';
-import style from './style.module.scss';
+import styles from './styles.module.scss';
 
 export function Header() {
   return (
-    <header className={style['container']}>
-      <FlexBox>
-        <a href="home">_hello</a>
-        <a href="about">_about</a>
-        <a href="services">_projects</a>
+    <header className={styles['container']}>
+      <FlexBox alignItems="center">
+        <FlexBox alignItems="center" className={styles['sider-bar-title']}>
+          <span>Thiago Borba</span>
+        </FlexBox>
+        <FlexBox>
+          <a href="home">_hello</a>
+        </FlexBox>
+        <FlexBox>
+          <a href="services">_projects</a>
+        </FlexBox>
+
+        <FlexBox>
+          <a href="about">_about</a>
+        </FlexBox>
       </FlexBox>
 
-      <div>
+      <FlexBox alignItems="center">
         <a href="#contact">_contact</a>
-      </div>
+      </FlexBox>
     </header>
   );
 }
+
+export default Header;
