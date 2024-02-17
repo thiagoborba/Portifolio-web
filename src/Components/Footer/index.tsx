4914d8c5d1ba982d4f8b77f2cab77f1c1575ad3e
thiago <thborba1@gmail.com>
2024-02-17 19:40:56 -0300

    feat: remove p de footer

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -1,11 +1,7 @@
 import styles from './styles.module.scss';
 
 export function Footer() {
-  return (
-    <footer className={styles['footer']}>
-      <p>Footer</p>
-    </footer>
-  );
+  return <footer className={styles['footer']}>Footer</footer>;
 }
 
 export default Footer;
