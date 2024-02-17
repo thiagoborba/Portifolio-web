33e84cef5ec1dcd176b07f7545e9b08525b8e08c
thiago <thborba1@gmail.com>
2024-02-17 05:54:05 -0300

    feat: cria index componentes

---
+++ src/Components/index.tsx
@@ -0,0 +1,4 @@
+export { FlexBox } from '@/Components/FlexBox';
+export { Header } from '@/Components/Header';
+export { SideNav } from '@/Components/SideNav';
+export { Footer } from '@/Components/Footer';
