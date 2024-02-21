bc437588c2c4b3ccdf1512a22a661fbfca55188f
thiago <thborba1@gmail.com>
2024-02-21 01:46:39 -0300

    feat: adiciona svg

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,4 +1,7 @@
+/// <reference types="vite-plugin-svgr/client" />
+
 import { SideNav, Header, Footer, Container } from '@/Components';
+import Background from '@/Assets/Background Blurs.svg?react';
 import './styles.scss';
 
 export function Layout() {
@@ -6,6 +9,16 @@ export function Layout() {
     <div className="outside-container">
       <Header />
       <main>
+        <Background
+          style={{
+            height: '78.9vh',
+            width: '43.7vw',
+            position: 'absolute',
+            right: '9.79vhw',
+            bottom: '9.01vh',
+            backgroundColor: 'transparent',
+          }}
+        />
         <SideNav />
         <Container />
       </main>
