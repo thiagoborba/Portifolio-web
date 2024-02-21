2bb03f0bd3789ff44173896cfcae97e09f0d52d5
thiago <thborba1@gmail.com>
2024-02-21 20:23:42 -0300

    feat: adiciona backgroud

--- src/Components/Container/index.tsx
+++ src/Components/Container/index.tsx
@@ -1,7 +1,16 @@
 import './styles.scss';
+import Background from '@/Assets/Background Blurs.svg?react';
 
 export function Container() {
-  return <div className="container">Container</div>;
+  return (
+    <div className="container">
+      <div className="container-left">container left</div>
+      <div className="container-right">
+        container right
+        <Background className="background" />
+      </div>
+    </div>
+  );
 }
 
 export default Container;
--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,7 +1,6 @@
 /// <reference types="vite-plugin-svgr/client" />
 
 import { SideNav, Header, Footer, Container } from '@/Components';
-import Background from '@/Assets/Background Blurs.svg?react';
 import './styles.scss';
 
 export function Layout() {
@@ -9,16 +8,6 @@ export function Layout() {
     <div className="outside-container">
       <Header />
       <main>
-        <Background
-          style={{
-            height: '78.9vh',
-            width: '43.7vw',
-            position: 'absolute',
-            right: '9.79vhw',
-            bottom: '9.01vh',
-            backgroundColor: 'transparent',
-          }}
-        />
         <SideNav />
         <Container />
       </main>
