bd12d90750d83a9b5ecbbaa56918836891dce919
thiago <thborba1@gmail.com>
2024-03-15 20:10:12 -0300

    feat: ajusta imports move para componentes o layout

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import '@/styles/_reset.scss';
 
-import { Layout } from '@/Layout';
+import { Layout } from '@/Components/Layout';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
